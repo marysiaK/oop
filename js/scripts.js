893aa750894f8f8a2c0228692879fb7e15d00e51
marysiaK <marysia.kolaczynska@gmail.com>
2017-01-11 17:00:20 +0100

    zadanie 10.4

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+﻿function Telefon(marka, cena, kolor) {
+	this.marka = marka; 
+	this.cena = cena;
+	this.kolor = kolor;
+}
+Telefon.prototype.printInfo = function() {
+		console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena + ".");
+}
+var iPhone6S = new Telefon("Apple", 2250, "srebrny");
+var SamsungGalaxy = new Telefon("Samsung", 3500, "gold");
+var OnePlus  = new Telefon("OnePLus", 2500, "czarny");
+
+iPhone6S.printInfo();
+SamsungGalaxy.printInfo();
+OnePlus.printInfo();
