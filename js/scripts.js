a4860ed6b08aa1647a9b3ee23a99ed4cffe3136d
marysiaK <marysia.kolaczynska@gmail.com>
2017-01-23 20:52:15 +0100

    dodana cena gwarancji

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,13 @@
 	this.marka = marka; 
 	this.cena = cena;
 	this.kolor = kolor;
-}
+};
 Telefon.prototype.printInfo = function() {
-		console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena + ".");
-}
+		console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena  + " Cena przedłużonej gwarancji  " + this.getWarranyCost() + "zł. ");
+};
+Telefon.prototype.getWarranyCost = function() {
+    return this.cena * 0.1;
+};
 var iPhone6S = new Telefon("Apple", 2250, "srebrny");
 var SamsungGalaxy = new Telefon("Samsung", 3500, "gold");
 var OnePlus  = new Telefon("OnePLus", 2500, "czarny");
